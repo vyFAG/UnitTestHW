c162efb37134cdcb671278055c5e4e42232736f4
vyFAG <vovchka02@inbox.ru>
2023-06-25 22:56:11 +0300

    error fixed

--- UnitTestHW.cpp
+++ UnitTestHW.cpp
@@ -1,20 +1,4 @@
-﻿#include <iostream>
-#include <cmath>
-#include "UnitTestHW.h"
-
-
-
-std::vector<double> SquareEquation::solve(double a, double b, double c)
-{
-    double discriminant = std::pow(b, 2) - 4 * a * c;
-
-    if (discriminant < 0)
-    {
-        return std::vector<double>();
-    }
-
-    return std::vector<double>();
-}
+﻿#include "UnitTestHW.h"
 
 
 
--- UnitTestHW.h
+++ UnitTestHW.h
@@ -1,11 +1,23 @@
 #pragma once
 
 #include <vector>
+#include <iostream>
+#include <cmath>
 
 
 
 class SquareEquation
 {
 public:
-    std::vector<double> solve(double a, double b, double c);
+    std::vector<double> solve(double a, double b, double c)
+    {
+        double discriminant = std::pow(b, 2) - 4 * a * c;
+
+        if (discriminant < 0)
+        {
+            return std::vector<double>();
+        }
+
+        return std::vector<double>();
+    }
 };
