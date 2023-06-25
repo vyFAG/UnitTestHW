51397b64841ff8309b29d9a8dd2c3cecda4fef97
vyFAG <vovchka02@inbox.ru>
2023-06-25 23:00:39 +0300

    error fixed

--- UnitTestHW.cpp
+++ UnitTestHW.cpp
@@ -2,6 +2,20 @@
 
 
 
+std::vector<double> SquareEquation::solve(double a, double b, double c)
+{
+    double discriminant = std::pow(b, 2) - 4 * a * c;
+
+    if (discriminant < 0)
+    {
+        return std::vector<double>();
+    }
+
+    return std::vector<double>();
+}
+
+
+
 int main()
 {
 }
--- UnitTestHW.h
+++ UnitTestHW.h
@@ -9,15 +9,5 @@
 class SquareEquation
 {
 public:
-    std::vector<double> solve(double a, double b, double c)
-    {
-        double discriminant = std::pow(b, 2) - 4 * a * c;
-
-        if (discriminant < 0)
-        {
-            return std::vector<double>();
-        }
-
-        return std::vector<double>();
-    }
+    std::vector<double> solve(double a, double b, double c);
 };
